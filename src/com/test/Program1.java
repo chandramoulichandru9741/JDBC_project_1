9ea4576d52b90a351637e58ae0233ebb0f6750d2
Chandramouli <108817253+chandramoulichandru9741@users.noreply.github.com>
2025-05-06 09:31:56 +0530

    Update Program1.java

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -1,9 +1,13 @@
 package com.test;
-
+import java.util.*;
 public class Program1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entera number");
+		int n = sc.nextInt();
+		System.out.println("Square of " + n + " is: " + (n * n));
 	}
 
 }
